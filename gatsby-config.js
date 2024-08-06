19b325c5db6abaf9fd975b6f988b2a3eb36ad3f8
grepo9 <grantc@supermicro.com>
2024-08-06 14:22:48 -0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Grant Chiu',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Grant Chiu is a software engineer',
     siteUrl: 'https://grantchiu.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
