ac35437db609094044714170052f41184676a79a
grantuiuc <grantc2@illinois.edu>
2026-02-20 22:46:42 -0800

    fix colors

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -7,10 +7,10 @@ const variables = css`
     --light-navy: #112240;
     --lightest-navy: #233554;
     --navy-shadow: rgba(2, 12, 27, 0.7);
-    --dark-slate: #495670;
-    --slate: #8892b0;
-    --light-slate: #a8b2d1;
-    --lightest-slate: #ccd6f6;
+    --dark-slate: #5a6580;
+    --slate: #98a2c0;
+    --light-slate: #b4bdd4;
+    --lightest-slate: #d4dcf5;
     --white: #e6f1ff;
     --green: #64ffda;
     --green-tint: rgba(100, 255, 218, 0.1);
